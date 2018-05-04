fd83acd84be4437176ea16e74d2cb973134abd5e
Alex Batalov <radist2s@gmail.com>
2018-05-04 19:57:49 +0500

    Node and Browser use different entry point

---
+++ src/index-node.js
@@ -0,0 +1,9 @@
+import { Route } from './route';
+import { RouterStore } from './router-store';
+import * as director from 'director';
+
+//components
+import { MobxRouter } from './components/MobxRouter';
+import { Link } from './components/Link';
+
+export { Route, MobxRouter, Link, RouterStore, director };
